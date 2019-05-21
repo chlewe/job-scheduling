8ab981f5d3d1be9a5455c6dd523d8a1f5e3aaf59
PattuX <patrick.wienhoeft@tu-dresden.de>
2019-05-21 09:51:20 +0200

    add parsing of scheduling tasks

--- main.py
+++ main.py
@@ -31,3 +31,57 @@ class Job:
             times += " "*(7-len(str(operation.time))) + str(operation.time)
             machines += " " * (7 - len(str(operation.machine))) + str(operation.machine)
         return operations + "\n" + times + "\n" + machines
+
+
+class SchedulingTask:
+
+    def __init__(self, jobs: List[Job] = None):
+        if jobs is None:
+            jobs = []
+        self.jobs = jobs
+
+    def add_job(self, job):
+        self.jobs.append(job)
+
+    def add_jobs(self, jobs):
+        for job in jobs:
+            self.jobs.append(job)
+
+    def add_from_file(self, path_to_file, replace_jobs = False):
+        """
+        Add all jobs from the given file to the scheduling instance.
+        Expected format is jobs separated by \n and operations given by machine time separated by blanks, e.g.
+        1 14 2 6 3 17
+        2 5 3 21 1 10
+
+        Keyword arguments:
+        path_to_file -- relative path to the text file to be parsed
+        replace_jobs -- boolean value whether to overwrite all existing jobs
+        """
+        with open(path_to_file) as f:
+            job_list = []
+            for line in f:
+                j = Job()
+                while line[0] == " ":
+                    line = line[1:]
+                while line[-1] == " ":
+                    line = line[:-1]
+                while "  " in line:
+                    line = line.replace("  ", " ")
+                line = line.split(" ")
+                if len(line)%2 != 0:
+                    print("Couldn't read file " + path_to_file + " as a job contained an odd number of values.")
+                    return
+                for (machine, time) in zip(line[::2], line[1::2]):
+                    op = Operation(time, machine)
+                    j.add_operation(op)
+                job_list.append(j)
+        if replace_jobs:
+            self.jobs = []
+        self.add_jobs(job_list)
+
+    def __str__(self):
+        out = ""
+        for i, job in enumerate(self.jobs):
+            out += "Job " + str(i) + "\n" + str(job) + "\n\n"
+        return out
