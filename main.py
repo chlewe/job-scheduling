e211d770c2bc23af09ecfa591726f69c5188a5bd
PattuX <patrick.wienhoeft@tu-dresden.de>
2019-05-21 13:42:10 +0200

    fiddle with parameters

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from annealing import *
 if __name__ == "__main__":
     scheduling_task = SchedulingTask()
     scheduling_task.add_from_file("tai_15_15_1.txt")
-    sa = SimulatedAnnealing(10,
+    sa = SimulatedAnnealing(20,
                             scheduling_task.random_schedule,
                             SchedulingTask.get_random_neighbour_arbitrary,
                             SchedulingTask.get_schedule_time)
     for i in range(200000):
+        if sa.time % 10000 == 1:
+            print(str((sa.time-1)/2000) + "%")
+            print(sa.evaluation_function(sa.state))
         sa.do_annealing_step()
-        if sa.time % 10000 == 0:
-            print(str(sa.time/2000) + "%")
     print(sa.state)
     print(sa.evaluation_function(sa.state))
